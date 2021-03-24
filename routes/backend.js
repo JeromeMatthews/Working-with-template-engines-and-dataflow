f0a8696b2537ac43594cde11e2dc0444fb645516
Jerome Matthews <jerome_matthews@live.com>
2021-03-23 23:16:18 -0500

    Added: Remaining Assignment Routes + grammar change to 404.html file
    Finished adding the remaining routes to the app, /users and 404 error route handler. Changed a typo in the 404.html page.  Added some comments for body-parser package usage in server.js file.

--- routes/backend.js
+++ routes/backend.js
@@ -8,6 +8,20 @@ appRoutes.get('/',(req, res, next) =>{
     console.log("server started, now listening on port: 5000");
     res.sendFile(path.join(customPath,"views", "index.html"));
 
+
+});
+
+
+appRoutes.get('/users', (req, res, next) =>{
+    console.log("Now acessing the users route...");
+    res.sendFile(path.join(customPath,"views", "users.html"));
+});
+
+
+appRoutes.use('/', (req, res, next) =>{
+    console.log("ERROR - URL cannot be found.");
+    res.sendFile(path.join(customPath,"views", "404.html"));
+    //Use the .use() function as a catch for any routes that aren't specified in the site/app.
 });
 
 module.exports = appRoutes; // exporting all the routes to the server.js file 
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require('path');// localizing path package from node.
 const customPath = require('./utils/path');// importing the custom path variable from utils to set the app root automatically.
 const appRoutes = require('./routes/backend');// importing the routes variable from routes.
 
+const bodyParser = require('body-parser');// pulling in the body-parser from the npm package install done. 
 
-
+server.use(bodyParser.urlencoded({extended: false})); // parsing the html data from the request body into text.
+//must happen before any of the middleware functions are accessed. So must be called before server.use(appRoutes).
+//================================================================
 
 
 server.use(appRoutes)// outsourced routes for the app. 
